5d4a8329e16993d9c0844c03a981c2bf6d3b2697
2sammy <samuelmanoah61@gmail.com>
2022-04-25 03:37:34 -0700

    doc: project added

---
+++ python-sql/lists.py
@@ -0,0 +1,15 @@
+// join 
+new_str = "\n".join(["fore", "aft", "starboard", "port"])
+print(new_str)
+
+name = "-".join(["García", "O'Kelly", "Davis"])
+print(name)
+
+append method
+letters = ['a', 'b', 'c', 'd']
+letters.append('z')
+print(letters)
+
+location = (13.4125, 103.866667)
+print("Latitude:", location[0])
+print("Longitude:", location[1])
